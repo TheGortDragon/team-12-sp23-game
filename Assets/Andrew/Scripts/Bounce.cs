fc18f8c670a28a069c055ecd7a8b55df21ef926b
TheGortDragon <Andrewhowell921@gmail.com>
2023-05-05 00:48:33 -0700

    added animations for the player

--- Assets/Andrew/Scripts/Bounce.cs
+++ Assets/Andrew/Scripts/Bounce.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bounce : MonoBehaviour {
+    private Animator anim;
     private Rigidbody2D rb2;
+    
     public float jumpBonus;
     public float jumpHeight;
     private float jumpHoldDuration;
@@ -14,6 +16,7 @@ public class Bounce : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         rb2 = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
         jumpHoldDuration = 0;
         angle = 0;
     }
@@ -36,7 +39,10 @@ public class Bounce : MonoBehaviour {
         if (jumpBoost) {
             rb2.AddForce(transform.up * jumpBonus);
             rb2.rotation = angle;
+            
         }
+        anim.SetBool("boost", jumpBoost);
+        anim.SetFloat("yVelocity", rb2.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -45,5 +51,10 @@ public class Bounce : MonoBehaviour {
         }
         rb2.AddForce(transform.up * jumpHeight);
         rb2.rotation = angle;
+        anim.SetBool("ground", true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        anim.SetBool("ground", false);
     }
 }
